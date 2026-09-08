72287b03c6c5207afa4d9ae838f846c941f22254
anastasia-stoltz <stoltzana@gmail.com>
2026-09-08 10:45:19 -0400

    changing to setlist

--- App_Design_Sound_of_America/pages/music_map.py
+++ App_Design_Sound_of_America/pages/music_map.py
@@ -12,7 +12,7 @@ import os
 
 #changed to allow for directory differences
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-df = pd.read_csv(os.path.join(BASE_DIR, "..", "assets", "concert_map_data.csv"))
+df = pd.read_csv(os.path.join(BASE_DIR, "..", "assets", "concert_map_data_setlistfm.csv"))
 
 years = sorted(df["year"].unique())
 genres = sorted(df["genre"].unique())
